6998e85d5396d0dc8a589c09176eedc4b8244884
Sergey Vlasov <sigprof@gmail.com>
2021-08-22 23:30:34 +0300

    WIP: Implement HID instead of the custom protocol
    Implement the same protocol as the LUFA HID bootloader (supports program
    memory writes only).

--- usbdesc.c
+++ usbdesc.c
@@ -27,6 +27,9 @@
 #define USB_DESCRIPTOR_TYPE_DEVICE          1
 #define USB_DESCRIPTOR_TYPE_CONFIGURATION   2
 #define USB_DESCRIPTOR_TYPE_INTERFACE       4
+#define USB_DESCRIPTOR_TYPE_ENDPOINT        5
+#define USB_DESCRIPTOR_TYPE_HID             0x21
+#define USB_DESCRIPTOR_TYPE_HID_REPORT      0x22
 #define USB_BCD_VERSION_11                  0x0110
 
 #define USBDESC(x) __attribute__((section(".usbdesc." x), used))
@@ -52,7 +55,7 @@ const struct USB_DeviceDescriptor dev_desc USBDESC("device") = {
   .bLength = sizeof(struct USB_DeviceDescriptor),
   .bDescriptorType = USB_DESCRIPTOR_TYPE_DEVICE,
   .bcdUSB = USB_BCD_VERSION_11,
-  .bDeviceClass = 0xff,
+  .bDeviceClass = 0,
   .bDeviceSubClass = 0,
   .bDeviceProtocol = 0,
   .bMaxPacketSize0 = MAX_PACKET,
@@ -85,12 +88,52 @@ struct USB_InterfaceDescriptor {
   uint8_t iInterface;
 } __attribute__((packed));
 
-struct USB_ConfigurationDescriptorSet {
+struct USB_HIDDescriptor {
+  uint8_t bLength;
+  uint8_t bDescriptorType;
+  uint16_t bcdHID;
+  uint8_t bCountryCode;
+  uint8_t bNumReportDescriptors;
+  uint8_t bReportDescriptorType;
+  uint16_t wReportDescriptorLength;
+} __attribute__((packed));
+
+struct USB_EndpointDescriptor {
+  uint8_t bLength;
+  uint8_t bDescriptorType;
+  uint8_t bEndpointAddress;
+  uint8_t bmAttributes;
+  uint16_t wMaxPacketSize;
+  uint8_t bInterval;
+} __attribute__((packed));
+
+/* The part of configuration descriptor set before the HID descriptor. */
+struct USB_ConfigurationDescriptorSet_1 {
   struct USB_ConfigurationDescriptor config;
   struct USB_InterfaceDescriptor ac_interface;
 } __attribute__((packed));
 
-const struct USB_ConfigurationDescriptorSet conf_desc USBDESC("config") = {
+/* The complete configuration descriptor set. */
+struct USB_ConfigurationDescriptorSet {
+  struct USB_ConfigurationDescriptorSet_1 first_part;
+  struct USB_HIDDescriptor hid;
+  struct USB_EndpointDescriptor hid_input_ep;
+} __attribute__((packed));
+
+const uint8_t hid_report_desc[] USBDESC("hid_report") = {
+  0x06, 0xDC, 0xFF,  // Usage Page (Vendor Defined 0xDCFF)
+  0x09, 0xFB,        // Usage (0xFB)
+  0xA1, 0x01,        // Collection (Application)
+  0x09, 0x02,        //   Usage (0x02)
+  0x15, 0x00,        //   Logical Minimum (0)
+  0x25, 0xFF,        //   Logical Maximum (255)
+  0x75, 0x08,        //   Report Size (8)
+  0x96, 0x82, 0x00,  //   Report Count (130) -> SPM_PAGESIZE (128 bytes) + 2
+  0x91, 0x02,        //   Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+  0xC0,              // End Collection
+};
+
+const struct USB_ConfigurationDescriptorSet_1 conf_desc USBDESC("config_1") = {
   .config = {
     .bLength = sizeof(struct USB_ConfigurationDescriptor),
     .bDescriptorType = USB_DESCRIPTOR_TYPE_CONFIGURATION,
@@ -105,10 +148,29 @@ const struct USB_ConfigurationDescriptorSet conf_desc USBDESC("config") = {
     .bDescriptorType = USB_DESCRIPTOR_TYPE_INTERFACE,
     .bInterfaceNumber = 0,
     .bAlternateSetting = 0,
-    .bNumEndpoints = 0,
-    .bInterfaceClass = 0xff,
+    .bNumEndpoints = 1,
+    .bInterfaceClass = 3,
     .bInterfaceSubClass = 0,
     .bInterfaceProtocol = 0,
     .iInterface = 0,
   },
+};
+
+const struct USB_HIDDescriptor hid_desc USBDESC("hid") = {
+  .bLength = sizeof(struct USB_HIDDescriptor),
+  .bDescriptorType = USB_DESCRIPTOR_TYPE_HID,
+  .bcdHID = 0x0111,
+  .bCountryCode = 0,
+  .bNumReportDescriptors = 1,
+  .bReportDescriptorType = USB_DESCRIPTOR_TYPE_HID_REPORT,
+  .wReportDescriptorLength = sizeof(hid_report_desc),
+};
+
+const struct USB_EndpointDescriptor hid_input_ep_desc USBDESC("hid_input_ep") = {
+  .bLength = sizeof(struct USB_EndpointDescriptor),
+  .bDescriptorType = USB_DESCRIPTOR_TYPE_ENDPOINT,
+  .bEndpointAddress = 0x81,
+  .bmAttributes = 0x03,
+  .wMaxPacketSize = MAX_PACKET,
+  .bInterval = 5,
 };
